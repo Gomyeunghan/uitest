ff596a589832a7f75d826fb56b17b26505e6f536
Gomyeunghan <rhaudgks04@gmail.com>
2024-05-02 11:52:49 +0900

    html 프로젝트 1일차

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 8086,
-  root: "src/",
+  root: "src/", //루트를 설정해놔서 src/를 루트로 인지하고 http://localhost:8086/index.html 로 보여짐 //
   open: ["index.html"],
   host: "localhost",
   browser: "chrome",
